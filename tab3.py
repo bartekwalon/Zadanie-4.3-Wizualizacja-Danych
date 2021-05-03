e4cf90c76e02530ef0e6138e9de23c3c78e1a4ae
bartekwalon <75641536+bartekwalon@users.noreply.github.com>
2021-05-03 12:37:18 +0200

    Zadanie 4.3 - wizualizacja danych sprzedazowych

---
+++ tab3.py
@@ -0,0 +1,20 @@
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+
+def render_tab(df):
+
+    grouped = df[df['total_amt']>0].groupby('Store_type')['total_amt'].sum()
+    fig = go.Figure(data=[go.Pie(labels=grouped.index,values=grouped.values)],layout=go.Layout(title='Udział kanałów sprzedaży w ogólnej sprzedaży'))
+
+    layout = html.Div([html.H1('Kanały sprzedaży - dni tygodnia',style={'text-align':'center'}),
+
+                        html.Div([html.Div([dcc.Graph(id='other-pie-store-type',figure=fig)],style={'width':'50%'}), 
+                        html.Div([dcc.Dropdown(id='store_type_dropdown', 
+                                    options=[{'label':Store_type,'value':Store_type} for Store_type in df['Store_type'].unique()],
+                                    value=df['Store_type'].unique()[0]),
+                                    dcc.Graph(id='bar-store-type')],style={'width':'50%'})],style={'display':'flex'}), 
+                                    html.Div(id='temp-out') 
+                        ])
+
+    return layout
